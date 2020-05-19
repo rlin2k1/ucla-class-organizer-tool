3228fb226fe6d1c9d4d7d20930f0002f6b1813cd
rlin2k1 <rlin2k1@gmail.com>
2020-05-19 16:02:33 -0700

    Adds comma in between mock classes.

--- src/app/mock-classes.ts
+++ src/app/mock-classes.ts
@@ -7,7 +7,7 @@ export const CLASSES: Class[] = [
   { id: 131, name: 'CS 131: Programming Languages' },
   { id: 134, name: 'CS 134: Distributed Systems' },
   { id: 144, name: 'CS 144: Web Applications' },
-  { id: 146, name: 'CS 146: Machine Learning' }
+  { id: 146, name: 'CS 146: Machine Learning' },
   { id: 161, name: 'CS 161: Artificial Intelligence' },
   { id: 180, name: 'CS 180: Algorithms and Complexity' },
   { id: 188, name: 'CS 188: Computer Vision' },
